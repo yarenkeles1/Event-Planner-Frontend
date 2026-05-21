0b8b4659dd3f4e4436a8136491d6e1f516ef37e1
yaren keleş <yarennkeles@gmail.com>
2026-05-22 02:08:27 +0300

    fetch and display joined events count from backend

--- src/app/profile/profile.ts
+++ src/app/profile/profile.ts
@@ -1,5 +1,6 @@
-import { Component, signal, OnInit } from '@angular/core';
+import { Component, signal, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-profile',
@@ -9,15 +10,31 @@ import { CommonModule } from '@angular/common';
   styleUrl: './profile.css'
 })
 export class Profile implements OnInit {
+  private http = inject(HttpClient); 
   
   fullName = signal<string>('');
   email = signal<string>('');
   
-  createdEventsCount = signal<number>(5);
-  joinedEventsCount = signal<number>(3);
+  createdEventsCount = signal<number>(0);
+  joinedEventsCount = signal<number>(0);
 
   ngOnInit() {
     this.fullName.set(sessionStorage.getItem('fullName') || 'Kullanıcı Bilgisi Yok');
     this.email.set(sessionStorage.getItem('email') || 'Email Bilgisi Yok');
+    this.fetchEventData();
+  }
+
+  fetchEventData() {
+    const options = { withCredentials: true };
+
+    this.http.get<any>('http://localhost:8081/event/myEvents', options)
+      .subscribe(res => {
+        if (res && res.totalElements !== undefined) {
+            this.createdEventsCount.set(res.totalElements);
+        }
+      });
+
+    this.http.get<number>('http://localhost:8081/event/countJoinedEvents', options)
+    .subscribe(count => this.joinedEventsCount.set(count));
   }
 }
